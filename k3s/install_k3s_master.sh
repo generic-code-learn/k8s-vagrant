dc9f23b8b4f5eb9af555f8c3d3c9778116dd3ab7
杨中肖 <yangzhongxiao@dfsoft.com.cn>
2024-06-14 10:09:55 +0800

    修改k3s脚本

--- k3s/install_k3s_master.sh
+++ k3s/install_k3s_master.sh
@@ -2,10 +2,6 @@
 
 set -euo pipefail
 
-# # k3s主服务器内网地址
-# export MY_SERVER_IP=172.21.0.11
-# # k3s认证令牌，任意字符串
-# export MY_TOKEN=943uoeitg4sh43589j38uj8irr439943oit
 # # 内部网卡设备号，`ifconfig` 查看
 # export MY_ETH_DEV=eth0
 # # 自动获取本机IP，若无法获取请手动指定
--- k3s/install_k3s_worker.sh
+++ k3s/install_k3s_worker.sh
@@ -2,12 +2,6 @@
 
 set -euo pipefail
 
-# # k3s主服务器内网地址
-# export MY_SERVER_IP=172.21.0.11
-# # k3s认证令牌，任意字符串
-# export MY_TOKEN=943uoeitg4sh43589j38uj8irr439943oit
-# # 内部网卡设备号，`ifconfig` 查看
-# export MY_ETH_DEV=eth0
 # # 自动获取本机IP，若无法获取请手动指定
 # export MY_NODE_IP=`ip -o -4 addr list | grep $MY_ETH_DEV | awk '{print $4}' | cut -d/ -f1`
 export iface=`ip -o -4 addr list | grep $ip | awk '{print $2}'`
